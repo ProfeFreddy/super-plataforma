a877eb914fb9d023de424e363d95a0e995798959
Freddy <profesorfreddy135@gmail.com>
2026-06-26 15:42:37 -0400

    Limpieza preventiva del repositorio

--- test.py
+++
@@ -1,5 +0,0 @@
-﻿import urllib.request
-url = "https://us-central1-pragma-2c5d1.cloudfunctions.net/api/ping"
-# Descargar el index.html correcto desde Firebase outputs
-req = urllib.request.Request("https://firebasestorage.googleapis.com")
-print("ok")
